2dbb2abf706885a18aa681b149c73bf1b9af2d92
ryota-k0827 <50436249+ryota-k0827@users.noreply.github.com>
2024-01-10 01:41:27 +0900

    Modify Domain name

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # password-generator
 
 ## 概要
+
 - パスワードをランダム値で生成するツールです。
 - 生成ルールやパスワード長もカスタマイズして使用することができます。
 
 ## 使用方法
-- GitHub Pagesで公開しています  
-- https://ryota-k0827.github.io/password-generator/
+
+- GitHub Pages で公開しています
+- https://password-generator.kryota.dev/
 
 ## ライセンス
+
 - [MIT](LICENSE)
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3333,
   },
   build: {
-    outDir: "./docs",
+    outDir: './docs',
   },
-  base: "https://ryota-k0827.github.io/password-generator/",
-});
+  base: 'https://password-generator.kryota.dev/',
+})
